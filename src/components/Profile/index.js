a519ed32cef475f454b09c6dc440cf545e27548b
Leandro Souza <leandrosouza0786@gmail.com>
2020-04-13 11:25:09 -0300

    add styled components, comps: avatar, layout and profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useStaticQuery, graphql} from 'gatsby'
 
+import Avatar from '../Avatar'
+import * as S from './styled'
 
 const Profile = () => {
     const {
@@ -18,11 +20,16 @@ const Profile = () => {
     `)
 
     return(
-        <div className="Profile-wrapper">
-            <h1>{title}</h1>
-            <h2>{position}</h2>
-            <p>{description}</p>
-        </div>
+        <S.ProfileWrapper>
+            <S.ProfileLink>
+                <Avatar />
+                <S.ProfileAuthor>
+                {title}
+                <S.ProfilePosition><h2>{position}</h2></S.ProfilePosition>
+                </S.ProfileAuthor>
+                <S.ProfileDescription>{description}</S.ProfileDescription>
+            </S.ProfileLink>
+        </S.ProfileWrapper>
     )
 }
 
